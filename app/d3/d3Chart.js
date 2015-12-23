4b2d70b9a03e81359d78d28cee3d08788c318e65
idelairre <idelairre@gmail.com>
2015-12-23 18:50:47 -0500

    minor formatting changes and graph tweaks

--- app/d3/d3Chart.js
+++ app/d3/d3Chart.js
@@ -1,10 +1,10 @@
 import articles from '../../output/articles';
 import backlinks from '../../output/backlinks';
 
-const width = 2000, height = 960;
-let labelDistance = 0;
+var width = 2000, height = 960;
+var labelDistance = 0;
 
-let d3Chart = {};
+var d3Chart = {};
 
 // links: { source: <nodeArrayPos>, target: <nodeArrayPos>, weight: <number> }
 // labelAnchorLinks: { node: { source: <nodeArrayPos>, target: <nodeArrayPos>, weight: <number> } };
@@ -21,32 +21,32 @@ var labelAnchors = [];
 
 console.log('initial data: ', backlinks.length);
 
-let articlesWithLinks = backlinks.filter((article) => {
-  if (article.links.length > 0) {
+var articlesWithLinks = backlinks.filter(function (article) {
+  if (article.links.length > 5) {
     return article;
   }
 });
 
 console.log('first pass: ', articlesWithLinks.length);
 
-articlesWithLinks.sort((a, b) => {
+articlesWithLinks.sort(function (a, b) {
   return a.title.toLowerCase() - b.title.toLowerCase();
 });
 
-for (let i = 0; i < articlesWithLinks.length - 1; i += 1) {
+for (var i = 0; i < articlesWithLinks.length - 1; i += 1) {
   if (articlesWithLinks[i].title.toLowerCase() === articlesWithLinks[i + 1].title.toLowerCase()) {
     delete articlesWithLinks[i];
   }
 }
 
-articlesWithLinks = articlesWithLinks.filter((element) => {
+articlesWithLinks = articlesWithLinks.filter(function (element) {
   return (typeof element !== "undefined");
 });
 
 console.log('second pass: ', articlesWithLinks.length);
 
-for(let i = 0; i < articlesWithLinks.length; i += 1) {
-	let node = {
+for (var i = 0; i < 150; i += 1) {
+	var node = {
 		label : articlesWithLinks[i].title
 	};
 	nodes.push(node);
@@ -58,14 +58,14 @@ for(let i = 0; i < articlesWithLinks.length; i += 1) {
 	});
 };
 
-for (let i = 0; i < nodes.length; i += 1) {
-  for (let j = 0; j < articlesWithLinks[i].links.length; j += 1) {
+for (var i = 0; i < nodes.length; i += 1) {
+  for (var j = 0; j < articlesWithLinks[i].links.length; j += 1) {
     nodes.map((node) => {
       if (node.label == articlesWithLinks[i].links[j].title) {
         links.push({
           source: i,
           target: nodes.indexOf(node),
-          weight: Math.random()
+          weight: articlesWithLinks[i].links.length * 0.01
         });
       }
     });
@@ -73,30 +73,30 @@ for (let i = 0; i < nodes.length; i += 1) {
   labelAnchorLinks.push({
     source : i * 2,
     target : i * 2 + 1,
-    weight : 1
+    weight : articlesWithLinks[i].links.length * 0.01
   });
 }
 
-let vis = d3.select('body')
+var vis = d3.select('body')
             .append('svg:svg')
             .attr('width', width)
             .attr('height', height);
 
-let force = d3.layout.force()
+var force = d3.layout.force()
               .size([width, height])
               .nodes(nodes)
               .links(links)
               .gravity(1)
               .linkDistance(50)
-              .charge(-3000)
+              .charge(-4000)
               .linkStrength(function (x) {
-                return x.weight * 10
+                return x.weight * 5
               });
 
  force.start();
 
 // graph that displays labels
-let force2 = d3.layout.force()
+var force2 = d3.layout.force()
                 .nodes(labelAnchors)
                 .links(labelAnchorLinks)
                 .gravity(0)
@@ -107,33 +107,33 @@ let force2 = d3.layout.force()
 
 force2.start();
 
-let link = vis.selectAll('line.link')
+var link = vis.selectAll('line.link')
               .data(links)
               .enter()
               .append('svg:line')
               .attr('class', 'link')
               .style('stroke', '#CCC');
 
-let node = vis.selectAll('g.node')
+var node = vis.selectAll('g.node')
               .data(force.nodes())
               .enter()
               .append('svg:g')
               .attr('class', 'node');
 
 node.append('svg:circle')
-    .attr('r', 5)
+    .attr('r', 7)
     .style('fill', '#555')
     .style('stroke', '#FFF')
     .style('stroke-width', 3);
 
 node.call(force.drag);
 
-let anchorLink = vis.selectAll('line.anchorLink')
+var anchorLink = vis.selectAll('line.anchorLink')
                     .data(labelAnchorLinks)
                     .enter()
                     .append('svg:line');
 
-let anchorNode = vis.selectAll('g.anchorNode')
+var anchorNode = vis.selectAll('g.anchorNode')
                     .data(force2.nodes())
                     .enter()
                     .append('svg:g')
@@ -150,10 +150,9 @@ anchorNode.append('svg:text')
 
 anchorNode.style('fill', '#555')
           .style('font-family', 'Arial')
-          .style('font-size', 12);
+          .style('font-size', 16);
 
-
-let updateLink = function () {
+var updateLink = function () {
   this.attr('x1', function (datum) {
     return datum.source.x;
   }).attr('y1', function (datum) {
@@ -165,12 +164,15 @@ let updateLink = function () {
   });
 }
 
-let updateNode = function () {
+var updateNode = function () {
   this.attr('transform', function (datum) {
-    // console.log('"this" context: ', this.attr, ' datum: ', datum);
     return 'translate(' + datum.x + ", " + datum.y + ')';
   });
 }
+
+anchorNode.on('click', function () {
+    window.location.href = `http://nosubject.com/${d3.select(this).text()}`;
+});
 
 force.on('tick', function () {
  force2.start();
